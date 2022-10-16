93acbb9a06f5dab9bf9ac673b0ddd5c25ccac1ee
Antonio Molina <antonio.molina@qustodio.com>
2022-10-16 14:07:06 +0200

    Initial base commit

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
